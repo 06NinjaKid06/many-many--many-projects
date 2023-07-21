73928300fb03ee465682b7f373843402f4269894
06NinjaKid06 <104595016+06NinjaKid06@users.noreply.github.com>
2023-07-21 16:41:20 +0000

    Add frame

--- Jul 20 23 sidebar test/main.py
+++ Jul 20 23 sidebar test/main.py
@@ -5,3 +5,8 @@ customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
 root.geometry("500x350") # Application width
+
+def login():
+    print("login")
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
