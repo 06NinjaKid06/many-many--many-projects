0225809d6704953368963b26dd7b37e7f5ee7941
06NinjaKid06 <104595016+06NinjaKid06@users.noreply.github.com>
2023-07-21 11:29:22 -0500

    Setup window so we can actually use the program

--- Jul 20 23 sidebar test/main.py
+++ Jul 20 23 sidebar test/main.py
@@ -1,1 +1,7 @@
 import customtkinter
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("500x350") # Application width
