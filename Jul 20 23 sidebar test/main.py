09b50fb672eb06fa75e71212f1b305ea8f758a64
06NinjaKid06 <104595016+06NinjaKid06@users.noreply.github.com>
2023-07-21 18:16:54 +0000

    Before Login System

--- Jul 20 23 sidebar test/main.py
+++ Jul 20 23 sidebar test/main.py
@@ -7,6 +7,17 @@ root = customtkinter.CTk()
 root.geometry("500x350") # Application width
 
 def login():
+    """Template Login System"""
     print("login")
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System", font=("Roboto", 24))
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="⁂")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
