e8af955afb2f56778d7904f01369922d3b8d9269
06NinjaKid06 <104595016+06NinjaKid06@users.noreply.github.com>
2023-07-27 20:47:48 +0000

    jar guessing game in rust

---
+++ rust/guessing-game/src/main.rs
@@ -0,0 +1,51 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+
+    println!("Welcome to the guessing game!");
+    println!("Type 'quit' to quit")
+
+    loop {
+
+        println!("Guess how many are in the jar! (1-999)");
+        println!("   /‾‾‾＼    ");
+        println!("  /ooooo＼   ");
+        println!(" |ooooooo|   ");
+        println!(" |ooooooo|   ");
+        println!(" |ooooooo|   ");
+        println!(" |_______|   ");
+
+        let secret_number = rand::thread_rng().gen_range(1..=999);
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        if guess == "quit" {
+            return "Bye!";
+        }
+
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        println!("It was: {secret_number}");
+
+    }
+
+}
